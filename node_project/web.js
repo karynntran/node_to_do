81415fdde6f162b997be66bdff27a1064c3e3c84
karynntran <karynn.tran@gmail.com>
2015-01-04 16:04:35 -0500

    Mapping requests through a static web server

---
+++ node_project/web.js
@@ -0,0 +1,42 @@
+//use NPM install to get mime, not included
+
+var path = require('path'),
+	http = require('http'),
+	fs = require('fs'),
+	url = require('url'),
+	mime = require('mime');
+
+var server = http.createServer();
+//needs to listen for a request
+
+//genericSend(404,"not found");
+function genericSend(code,message,response){
+	response.writeHead(code,{"Content-Type":"text/plain"});
+	response.end(message);
+};
+	
+server.on('request', function(request,response){
+	//404
+	//500
+	//200 OK
+	var urlParams = url.parse(request.url),
+	//pathname: /index.html ==> index.html
+		filename = path.join('.', urlParams.pathname);
+
+	path.exists(filename,function(exists){
+		if(!exists)
+			return genericSend(404,'not found',response);
+
+		fs.readFile(filename,'binary',function(err,file){
+			if(err)
+				return genericSend(500,'internal server error',response);
+
+			var type = mime.lookup(filename);
+			response.writeHead(200,{'Content-Type': type});
+			response.write(file,'binary');
+			response.end();
+		});	
+	});
+});
+
+server.listen(9000);
